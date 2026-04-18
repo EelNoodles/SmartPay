315e327c923b9c3449626925c356adce7d88126b
SamLin <samlin@auresys.biz>
2026-04-18 17:46:59 +0800

    chore: update application port to 6188 and generate package-lock.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,11 +9,11 @@ module.exports = {
       max_memory_restart: '512M',
       env: {
         NODE_ENV: 'development',
-        PORT: 3000
+        PORT: 6188
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 6188
       },
       error_file: './logs/err.log',
       out_file: './logs/out.log',
